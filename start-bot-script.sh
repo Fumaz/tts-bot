318e38803c2adde0822a0c4227b3d2df814a176b
Fumaz <mail@fumaz.dev>
2023-03-05 00:16:25 +0100

    remove debug

--- start-bot-script.sh
+++ start-bot-script.sh
@@ -1,1 +1,1 @@
-rm -rf /usr/src/app/node_modules/; npm install; npx prisma migrate deploy; npx prisma generate; rm -rf dist; DEBUG="grammy*" npm run start-bot
+rm -rf /usr/src/app/node_modules/; npm install; npx prisma migrate deploy; npx prisma generate; rm -rf dist; npm run start-bot
